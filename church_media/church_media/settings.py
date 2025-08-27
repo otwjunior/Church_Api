9e47dc921b9b5ce6bfd22b61feb1bdbe7939d6b1
otwjunior <jamesthebark@proton.me>
2025-08-27 18:29:57 +0300

    added render url to ALLOED HOST

--- church_media/church_media/settings.py
+++ church_media/church_media/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://church-api-862r.onrender.com']
 
 
 # Application definition
